9c90a8e2cffa9856cc9bf5875b7aa7562cd4f1de
My Name <mwara95@gmail.com>
2022-11-29 12:28:43 +0000

    Update Logo

---
+++ app/src/main/java/com/amazonaws/elb/ap_southeast_1/proffilix_2113527495/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.amazonaws.elb.ap_southeast_1.proffilix_2113527495.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
